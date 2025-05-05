c46930939691e9a164a58b35ad9bfd0351164698
Evan <h.yuan667@gmail.com>
2025-05-05 18:20:32 +0800

    add basic map reduce implementation

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,33 +1,153 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
-
+import (
+	"container/list"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"sync"
+	"time"
+)
 
 type Coordinator struct {
-	// Your definitions here.
+	mapTasks    *list.List    // ready Tasks list for Map tasks
+	reduceTasks map[int]*Task // ready Tasks map bteween reducer index and its tasks for Reduce tasks
 
+	runningTasks  map[int]*Task // map between task id and Task
+	completeTasks []*Task       // completed Tasks list
+
+	mu          sync.Mutex
+	idGenerator func() int
 }
+
+type Task struct {
+	TaskID     int
+	AttemptID  int
+	TaskType   TaskType
+	MapTask    *MapTask
+	ReduceTask *ReduceTask
+	Status     TaskStatus
+	ExpiredAt  time.Time
+}
+
+type TaskStatus int
+
+const (
+	Ready TaskStatus = iota
+	Running
+	Completed
+)
 
 // Your code here -- RPC handlers for the worker to call.
 
-//
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
 
+func (c *Coordinator) AcquireTask(args *ExampleArgs, taskDef *TaskDefinition) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	// check if there are any tasks in the map task list
+	if c.mapTasks.Len() != 0 {
+		// move the first task from the readyTasks list to the runningTasks map
+		// and update the task status to Running
+		// and set the expired time to 10 seconds from now
+		task := c.mapTasks.Remove(c.mapTasks.Front()).(*Task)
+		task.Status = Running
+		task.AttemptID = c.idGenerator()
+		// set the expired time to 10 seconds from now
+		task.ExpiredAt = time.Now().Add(10 * time.Second)
+		c.runningTasks[task.TaskID] = task
+		// set the task definition
+		taskDef.TaskID = task.TaskID
+		taskDef.AttemptID = task.AttemptID
+		taskDef.TaskType = task.TaskType
+		taskDef.MapTask = task.MapTask
+		taskDef.ReduceTask = task.ReduceTask
+	} else {
+		// check if there are any running map tasks
+		var numOfRunningMapTasks int = 0
+		for _, task := range c.runningTasks {
+			if task.TaskType == MAP {
+				numOfRunningMapTasks++
+			}
+		}
+		if numOfRunningMapTasks == 0 {
+			// if all the map tasks are completed, then we can start the reduce tasks
+			if len(c.reduceTasks) != 0 {
+				// select the first reduce task from the reduceTasks map
+				for _, task := range c.reduceTasks {
+					task.Status = Running
+					task.AttemptID = c.idGenerator()
+					// set the expired time to 10 seconds from now
+					task.ExpiredAt = time.Now().Add(10 * time.Second)
+					c.runningTasks[task.TaskID] = task
+					// set the task definition
+					taskDef.TaskID = task.TaskID
+					taskDef.AttemptID = task.AttemptID
+					taskDef.TaskType = task.TaskType
+					taskDef.MapTask = task.MapTask
+					taskDef.ReduceTask = task.ReduceTask
+					delete(c.reduceTasks, task.ReduceTask.reducerIndex)
+					break
+				}
+			}
+		}
+		// if there are no task for map and reduce, then we can return Terminal type
+		// Task to worker to ask it to exit
+		if len(c.reduceTasks) == 0 && len(c.runningTasks) == 0 {
+			taskDef.TaskType = TERMINAL
+		}
+	}
+	return nil
+}
 
-//
+func (c *Coordinator) SendTaskResult(taskResult *TaskResult, reply *ExampleReply) error {
+	// may not need to do anything here
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	// update the task status to Completed
+	task := c.runningTasks[taskResult.TaskID]
+	// make sure the attampt id is the same
+	if task != nil && task.AttemptID == taskResult.AttemptID {
+		task.Status = Completed
+		c.completeTasks = append(c.completeTasks, task)
+		delete(c.runningTasks, taskResult.TaskID)
+
+		if task.TaskType == MAP {
+			// generate reduce task and add it to the reduceTasks map
+			c.mergeReduceTaskForReducer(taskResult)
+		}
+	}
+	return nil
+}
+
+func (c *Coordinator) mergeReduceTaskForReducer(mapTaskResult *TaskResult) error {
+	for idx, file := range mapTaskResult.FilePaths {
+		if _, ok := c.reduceTasks[idx]; !ok {
+			c.reduceTasks[idx] = &Task{
+				TaskID:   c.idGenerator(),
+				TaskType: REDUCE,
+				ReduceTask: &ReduceTask{
+					reducerIndex:       idx,
+					intermedicateFiles: []string{file},
+				},
+				Status: Ready,
+			}
+		} else {
+			c.reduceTasks[idx].ReduceTask.intermedicateFiles = append(c.reduceTasks[idx].ReduceTask.intermedicateFiles, file)
+		}
+	}
+	return nil
+}
+
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -41,30 +161,50 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
 	ret := false
 
 	// Your code here.
 
-
 	return ret
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
-
+	c := Coordinator{
+		mapTasks:      list.New(),
+		reduceTasks:   make(map[int]*Task),
+		runningTasks:  make(map[int]*Task),
+		completeTasks: []*Task{},
+		idGenerator:   idGenerator(),
+	}
 	// Your code here.
-
-
+	for _, file := range files {
+		c.mapTasks.PushBack(&Task{
+			TaskID:   c.idGenerator(),
+			TaskType: MAP,
+			MapTask: &MapTask{
+				filePath: file,
+				nReduce:  nReduce,
+			},
+			Status: Ready,
+		})
+	}
 	c.server()
 	return &c
+}
+
+func idGenerator() func() int {
+	var mu sync.Mutex
+	id := 0
+	return func() int {
+		mu.Lock()
+		defer mu.Unlock()
+		id++
+		return id
+	}
 }
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,48 +1,160 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
+import (
+	"fmt"
+	"hash/fnv"
+	"io/ioutil"
+	"log"
+	"net/rpc"
+	"os"
+	"strings"
+	"time"
+)
 
-
-//
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
+type TaskDefinition struct {
+	TaskID     int
+	AttemptID  int
+	TaskType   TaskType
+	MapTask    *MapTask
+	ReduceTask *ReduceTask
+}
+
+type TaskResult struct {
+	TaskID    int
+	AttemptID int
+	TaskType  TaskType
+	FilePaths []string
+}
+
+type TaskType int
+
+const (
+	MAP TaskType = iota
+	REDUCE
+	TERMINAL
+)
+
+type MapTask struct {
+	filePath string
+	content  string
+	nReduce  int // the number of reduce tasks
+}
+
+type ReduceTask struct {
+	reducerIndex       int      // the key bucket calculated by ihash(key) % NReduce
+	intermedicateFiles []string // each file store key value pairs
+}
+
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
-//
 // main/mrworker.go calls this function.
-//
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
 	// Your worker implementation here.
 
 	// uncomment to send the Example RPC to the coordinator.
-	// CallExample()
+	for {
+		task := getTask()
+		if task == nil {
+			// connection failed, may sleep for a while and try again
+			time.Sleep(time.Second)
+			continue
+		}
+		switch task.TaskType {
+		case MAP:
+			performMapTask(task, mapf)
+		case REDUCE:
+			performReduceTask(task, reducef)
+		case TERMINAL:
+			// no more tasks, exit
+			log.Printf("no more tasks, exit")
+			return
+		default:
+			log.Fatalf("unknown task type %v", task.TaskType)
+		}
+
+	}
 
 }
 
-//
+func readFile(filename string) string {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("cannot open %v", filename)
+	}
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("cannot read %v", filename)
+	}
+	file.Close()
+	return string(content)
+}
+
+func performMapTask(task *TaskDefinition, mapf func(string, string) []KeyValue) {
+	// do map task
+	mapTask := task.MapTask
+	content := readFile(mapTask.filePath)
+	values := mapf(mapTask.filePath, content)
+
+	filePathPrefix := fmt.Sprintf("mr-%d-%d", task.TaskID, task.AttemptID)
+	resultFilePaths := saveKeyValueToFiles(filePathPrefix, mapTask.nReduce, values)
+	SendMapTaskResult(task.TaskID, task.AttemptID, resultFilePaths)
+}
+
+func performReduceTask(task *TaskDefinition, reducef func(string, []string) string) {
+	// do reduce task
+	reduceTask := task.ReduceTask
+	intermediateFiles := reduceTask.intermedicateFiles
+	// read the files and merge them into a map
+	kvMap := make(map[string][]string)
+	for _, filePath := range intermediateFiles {
+		content := readFile(filePath)
+		lines := strings.Split(content, "\n")
+		for _, line := range lines {
+			if line == "" {
+				continue
+			}
+			kv := strings.Split(line, " ")
+			if len(kv) != 2 {
+				log.Fatalf("invalid key value pair %v", line)
+			}
+			key := kv[0]
+			value := kv[1]
+			kvMap[key] = append(kvMap[key], value)
+		}
+	}
+
+	resultFilePath := fmt.Sprintf("mr-out-%d-%d", task.TaskID, task.AttemptID)
+	resultFile, err := os.Create(resultFilePath)
+	if err != nil {
+		log.Fatalf("cannot create %v", resultFilePath)
+	}
+	defer resultFile.Close()
+
+	for key, values := range kvMap {
+		resultValue := reducef(key, values)
+		resultFile.WriteString(fmt.Sprintf("%v %v\n", key, resultValue))
+	}
+
+	SendReduceTaskResult(task.TaskID, task.AttemptID, resultFilePath)
+}
+
 // example function to show how to make an RPC call to the coordinator.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func CallExample() {
 
 	// declare an argument structure.
@@ -67,11 +179,110 @@ func CallExample() {
 	}
 }
 
-//
+func getTask() *TaskDefinition {
+
+	// declare an argument structure.
+	taskDef := TaskDefinition{}
+
+	// send the RPC request, wait for the reply.
+	// the "Coordinator.Example" tells the
+	// receiving server that we'd like to call
+	// the Example() method of struct Coordinator.
+	ok := call("Coordinator.AcquireTask", &ExampleArgs{}, &taskDef)
+	if ok {
+		// reply.Y should be 100.
+		fmt.Printf("get Task %v\n", taskDef)
+		return &taskDef
+	} else {
+		fmt.Printf("call failed!\n")
+		return nil
+	}
+}
+
+// splite the values into different files
+// and return the file paths
+// the key is put in ith file where i = ihash(key) % nReduce
+func saveKeyValueToFiles(fileNamePrefix string, nReduce int, values []KeyValue) []string {
+	// split the values into different files
+	intermidiateFiles := make([]string, nReduce)
+	filePointer := make([]*os.File, nReduce)
+	for i := 0; i < nReduce; i++ {
+		intermidiateFiles[i] = fmt.Sprintf("%s-%d", fileNamePrefix, i)
+		file, err := os.OpenFile(
+			intermidiateFiles[i], os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+		if err != nil {
+			log.Fatalf("cannot open %v", intermidiateFiles[i])
+		}
+		filePointer[i] = file
+		defer filePointer[i].Close()
+	}
+	for _, v := range values {
+		// write the values to a file
+		fileIndex := ihash(v.Key) % nReduce
+		file := filePointer[fileIndex]
+		_, err := file.WriteString(fmt.Sprintf("%s %s\n", v.Key, v.Value))
+		if err != nil {
+			log.Fatalf("cannot write to %v", intermidiateFiles[fileIndex])
+		}
+	}
+	return intermidiateFiles
+}
+
+func SendMapTaskResult(taskId int, attemptID int, resultFilePaths []string) {
+
+	// declare an argument structure.
+	taskResult := TaskResult{
+		TaskID:    taskId,
+		AttemptID: attemptID,
+		TaskType:  MAP,
+		FilePaths: resultFilePaths,
+	}
+
+	// declare a reply structure.
+	reply := ExampleReply{}
+
+	// send the RPC request, wait for the reply.
+	// the "Coordinator.Example" tells the
+	// receiving server that we'd like to call
+	// the Example() method of struct Coordinator.
+	ok := call("Coordinator.SendTaskResult", &taskResult, &reply)
+	if ok {
+		// reply.Y should be 100.
+		fmt.Printf("send Map TaskResult %v\n", taskResult)
+	} else {
+		fmt.Printf("call failed!\n")
+	}
+}
+
+func SendReduceTaskResult(taskId int, attemptID int, resultPath string) {
+
+	// declare an argument structure.
+	taskResult := TaskResult{
+		TaskID:    taskId,
+		AttemptID: attemptID,
+		TaskType:  REDUCE,
+		FilePaths: []string{resultPath},
+	}
+
+	// declare a reply structure.
+	reply := ExampleReply{}
+
+	// send the RPC request, wait for the reply.
+	// the "Coordinator.Example" tells the
+	// receiving server that we'd like to call
+	// the Example() method of struct Coordinator.
+	ok := call("Coordinator.SendTaskResult", &taskResult, &reply)
+	if ok {
+		// reply.Y should be 100.
+		fmt.Printf("send Reduce TaskResult %v\n", taskResult)
+	} else {
+		fmt.Printf("call failed!\n")
+	}
+}
+
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
